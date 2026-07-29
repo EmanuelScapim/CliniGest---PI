cd501b9f128b8f37f922f7810ea0351069e9f6f4
EmanuelScapim <emanuelrrscapim@gmail.com>
2026-07-29 16:59:12 -0300

    feat: cria entidade Tratamento

---
+++ src/main/java/io/github/projetopi/pi/model/Tratamento.java
@@ -0,0 +1,23 @@
+package io.github.projetopi.pi.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.UUID;
+
+public class Tratamento {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "nomeTratamento", length = 50, nullable = false)
+    private String nomeTratamento;
+
+    @Column(name = "descricao", nullable = false)
+    private String descricao;
+
+}
