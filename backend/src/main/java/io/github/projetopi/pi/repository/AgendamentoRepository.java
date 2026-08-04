5cbf28f2d5eee29bded7361a63b68c0ca5942c37
EmanuelScapim <emanuelrrscapim@gmail.com>
2026-08-04 15:53:27 -0300

    feat: Cria repository Agendamento

--- backend/src/main/java/io/github/projetopi/pi/repository/AgendamentoRepository.java
+++ backend/src/main/java/io/github/projetopi/pi/repository/AgendamentoRepository.java
@@ -1,9 +1,24 @@
 package io.github.projetopi.pi.repository;
 
 import io.github.projetopi.pi.model.Agendamento;
+import io.github.projetopi.pi.model.Dentista;
+import io.github.projetopi.pi.model.Paciente;
+import io.github.projetopi.pi.model.Tratamento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface AgendamentoRepository extends JpaRepository<Agendamento, UUID> {
+
+    @Query("select d from Agendamento a join a.dentista d")
+    List<Dentista> listarDentistasDosAtendimentos();
+
+    @Query("select p from Agendamento a join a.paciente p")
+    List<Paciente> listarPacienteDosAtendimentos();
+
+    @Query("select t from Agendamento a join a.tratamento t")
+    List<Tratamento> listarTratamentoDosAtendimentos();
+
 }
