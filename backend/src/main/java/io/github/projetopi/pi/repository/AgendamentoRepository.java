abe4b8ef4fe35b90eef9e5ea4d555f7266a04977
EmanuelScapim <emanuelrrscapim@gmail.com>
2026-09-02 20:56:13 -0300

    feat: adiciona queries de agendamento ativo por paciente e dentista

--- backend/src/main/java/io/github/projetopi/pi/repository/AgendamentoRepository.java
+++ backend/src/main/java/io/github/projetopi/pi/repository/AgendamentoRepository.java
@@ -4,14 +4,20 @@ import io.github.projetopi.pi.model.Agendamento;
 import io.github.projetopi.pi.model.Dentista;
 import io.github.projetopi.pi.model.Paciente;
 import io.github.projetopi.pi.model.Tratamento;
+import io.github.projetopi.pi.model.enums.StatusAgendamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
 public interface AgendamentoRepository extends JpaRepository<Agendamento, UUID> {
+
+    boolean existsByPacienteIdAndStatusAgendamentoIn(UUID pacienteId, Collection<StatusAgendamento> status);
+
+    boolean existsByDentistaIdAndStatusAgendamentoIn(UUID dentistaId, Collection<StatusAgendamento> status);
 
     @Query("select d from Agendamento a join a.dentista d")
     List<Dentista> listarDentistasDosAtendimentos();
