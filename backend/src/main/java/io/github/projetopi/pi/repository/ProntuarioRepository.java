319e5804de2fb356a708f441505a0e479c902128
EmanuelScapim <emanuelrrscapim@gmail.com>
2026-09-02 20:53:10 -0300

    feat: adiciona existsByPacienteId no ProntuarioRepository

--- backend/src/main/java/io/github/projetopi/pi/repository/ProntuarioRepository.java
+++ backend/src/main/java/io/github/projetopi/pi/repository/ProntuarioRepository.java
@@ -6,4 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface ProntuarioRepository extends JpaRepository<Prontuario, UUID> {
+
+    boolean existsByPacienteId(UUID pacienteId);
+
 }
